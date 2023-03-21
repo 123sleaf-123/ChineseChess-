fc32745b16bbf92fd8ef3c0072defe4476a4eb0a
sleaf <huangzhiye2002@163.com>
2023-03-21 21:25:05 +0800

    抽象棋
    盒批寄乐赢麻退
    vs
    创蚌典孝急对绝

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+
 #include "new_game.h"
 
 int main(int argc, char const *argv[])
--- src/tools.c
+++ src/tools.c
@@ -14,17 +14,48 @@ char *c2tUser(struct ChessBoard* board, int code) {
  * @return char* 棋子中文名字
  */
 char *chessName(struct Chess* chess) {
+    // switch(chess->owner) {
+    //     // 玩家一是红色
+    //     case PLAYER_1: {
+    //         switch(chess->type) {
+    //             case GENERAL: return "帥";
+    //             case CHARIOT: return "俥";
+    //             case HORSE: return "傌";
+    //             case ARTILLERY: return "炮";
+    //             case ELEPHANT: return "相";
+    //             case WARRIOR: return "仕";
+    //             case SOLDIER: return "兵";
+    //         }
+    //         break;
+    //     }
+
+    //     // 玩家二是绿色
+    //     case PLAYER_2: {
+    //         switch(chess->type) {
+    //             case GENERAL: return "将";
+    //             case CHARIOT: return "車";
+    //             case HORSE: return "馬";
+    //             case ARTILLERY: return "砲";
+    //             case ELEPHANT: return "象";
+    //             case WARRIOR: return "士";
+    //             case SOLDIER: return "卒";
+    //         }
+    //         break;
+    //     }
+    // }
+
+    // 整活
     switch(chess->owner) {
         // 玩家一是红色
         case PLAYER_1: {
             switch(chess->type) {
-                case GENERAL: return "帥";
-                case CHARIOT: return "俥";
-                case HORSE: return "傌";
-                case ARTILLERY: return "炮";
-                case ELEPHANT: return "相";
-                case WARRIOR: return "仕";
-                case SOLDIER: return "兵";
+                case GENERAL: return "蚌";
+                case CHARIOT: return "创";
+                case HORSE: return "孝";
+                case ARTILLERY: return "急";
+                case ELEPHANT: return "典";
+                case WARRIOR: return "绝";
+                case SOLDIER: return "对";
             }
             break;
         }
@@ -32,17 +63,17 @@ char *chessName(struct Chess* chess) {
         // 玩家二是绿色
         case PLAYER_2: {
             switch(chess->type) {
-                case GENERAL: return "将";
-                case CHARIOT: return "車";
-                case HORSE: return "馬";
-                case ARTILLERY: return "砲";
-                case ELEPHANT: return "象";
-                case WARRIOR: return "士";
-                case SOLDIER: return "卒";
+                case GENERAL: return "赢";
+                case CHARIOT: return "盒";
+                case HORSE: return "批";
+                case ARTILLERY: return "麻";
+                case ELEPHANT: return "寄";
+                case WARRIOR: return "乐";
+                case SOLDIER: return "退";
             }
             break;
         }
     }
 
-    return " ";
+    return "锟";
 }
