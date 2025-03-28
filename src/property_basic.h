4f162c1d56cbc06c2940738034caa5cb689ff876
sleaf <huangzhiye2002@163.com>
2025-03-28 20:34:39 +0800

    加入了基础属性，但还没使用

---
+++ src/property_basic.h
@@ -0,0 +1,19 @@
+#ifndef PROPERTY_BASIC_H
+#define PROPERTY_BASIC_H
+
+typedef struct {
+    int strength;      // 力量
+    int magic;         // 魔力
+    int skill;         // 技巧
+    int speed;         // 速度
+    int luck;          // 幸运
+    int defense;       // 防守
+    int magic_defense;  // 魔防
+    int charm;         // 魅力
+    int movement;      // 移动力
+    int health;        // 生命值
+    int level;         // 等级
+    int experience;    // 经验值
+} BasicProperty;
+
+#endif
