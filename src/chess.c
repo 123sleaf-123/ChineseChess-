7c57e446f56b7a880c5672c0eb9c656626703e7f
sleaf <huangzhiye2002@163.com>
2025-04-03 18:30:59 +0800

    新增棋子攻击范围的最小和最大值支持，更新相关获取和设置函数

--- src/chess.c
+++ src/chess.c
@@ -1,6 +1,5 @@
-#include "global.h"
-#include "property_fight.h"
 #include "chess.h"
+#include "property_fight.h"
 
 int count = 0;
 
@@ -32,20 +31,42 @@ int getChessMovement(struct Chess *chess) {
     return chess->battle_property->movement;
 }
 
+int getChessMinRange(struct Chess *chess) {
+    return getAttackMinRange(getBattleProperty(chess));
+}
+
+void setChessMinRange(struct Chess *chess, int min_range) {
+    setAttackMinRange(getBattleProperty(chess), min_range);
+}
+
+int getChessMaxRange(struct Chess *chess) {
+    return getAttackMaxRange(getBattleProperty(chess));
+}
+
+void setChessMaxRange(struct Chess *chess, int max_range) {
+    setAttackMaxRange(getBattleProperty(chess), max_range);
+}
+
 bool isInsideAttackRangeByChess(struct Chess *attacker, struct Chess *defender) {
     struct Position *atttacker_pos = getChessPosition(attacker);
     struct Position *defender_pos = getChessPosition(defender);
-    int attack_range = getAttackRange(attacker->battle_property);
-    return isInsideAttackRangeByPos(atttacker_pos->x, atttacker_pos->y, defender_pos->x, defender_pos->y, attack_range);
+    int min_range = getAttackMinRange(attacker->battle_property);
+    int max_range = getAttackMaxRange(attacker->battle_property);
+    return isInsideAttackRangeByPos(atttacker_pos->x, atttacker_pos->y, defender_pos->x, defender_pos->y, min_range, max_range);
 }
 
-bool isInsideAttackRangeByPos(int attacker_row, int attacker_col, int defender_row, int defender_col, int range) {
-    return (abs(defender_row - attacker_row) + abs(defender_col - attacker_col) <= range);
+bool isInsideAttackRangeByPos(int attacker_row, int attacker_col, int defender_row, int defender_col, int min_range, int max_range) {
+    int distance = abs(attacker_row - defender_row) + abs(attacker_col - defender_col);
+    return (distance >= min_range && distance <= max_range);
 }
 
 void printChessBattleProperty(struct Chess *chess) {
     if (chess == NULL || chess->battle_property == NULL) return;
     BattleProperty *bp = chess->battle_property;
     printBattleProperty(bp);
+}
+
+BattleProperty *getBattleProperty(struct Chess *chess) {
+    return chess->battle_property;
 }
 
--- src/chess.h
+++ src/chess.h
@@ -19,9 +19,15 @@ typedef struct Chess
 struct Chess *initChess(int type, int owner, int is_alive);
 struct Position *initPosition(int x, int y);
 struct Position *getChessPosition(struct Chess *chess);
+
 int getChessMovement(struct Chess *chess);
+int getChessMinRange(struct Chess *chess);
+void setChessMinRange(struct Chess *chess, int min_range);
+int getChessMaxRange(struct Chess *chess);
+void setChessMaxRange(struct Chess *chess, int max_range);
 bool isInsideAttackRangeByChess(struct Chess *attacker, struct Chess *defender);
-bool isInsideAttackRangeByPos(int attacker_row, int attacker_col, int defender_row, int defender_col, int range);
+bool isInsideAttackRangeByPos(int attacker_row, int attacker_col, int defender_row, int defender_col, int min_range, int max_range);
 void printChessBattleProperty(struct Chess *chess);
+BattleProperty *getBattleProperty(struct Chess *chess);
 
 #endif
