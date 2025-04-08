98c1f27df6a398c86163039db1ce0ae52e8945b1
sleaf <huangzhiye2002@gmail.com>
2025-04-09 02:12:15 +0800

    补充函数声明

--- src/weapon.h
+++ src/weapon.h
@@ -1,7 +1,7 @@
 #ifndef WEAPON_H
 #define WEAPON_H
 
-#include "property_basic.h"
+#include "global.h"
 
 typedef struct {
     char *name;
@@ -14,5 +14,7 @@ typedef struct {
 
 void initWeapon(Weapon *weapon, char *name, int attack_power, int hit, int critical, int min_range, int max_range);
 Weapon *createWeapon(char *name, int attack_power, int hit, int critical, int min_range, int max_range);
+Weapon *createWeaponDeault();
+Weapon *createWeaponPrefab(char *name);
 
 #endif
