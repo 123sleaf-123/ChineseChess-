c95cf25800865cc6e429d9ef125da53c76272662
sleaf <huangzhiye2002@163.com>
2025-04-09 17:08:30 +0800

    增加背包支持和新的角色创建函数

--- src/character.c
+++ src/character.c
@@ -16,11 +16,8 @@ void initCharacterV2(Character *character, const char *name, BasicProperty *prop
     character->property_fight = createBattlePropertyFromBasicProperty(property_basic);
 }
 
-void initCharacterV3(Character *character, const char *name, BasicProperty *property_basic,
-                     BattleProperty *property_fight, Knapsack *knapsack) {
-    strcpy(character->name, name);
-    character->property_basic = property_basic;
-    character->property_fight = property_fight;
+void initCharacterV3(Character *character, const char *name, BasicProperty *property_basic, Knapsack *knapsack) {
+    initCharacterV2(character, name, property_basic);
     character->knapsack = knapsack;
 }
 
@@ -31,6 +28,22 @@ Character *createCharacter(const char *name, BasicProperty *property_basic, Batt
         exit(EXIT_FAILURE);
     }
     initCharacter(character, name, property_basic, property_fight);
+    return character;
+}
+
+Character *createCharacterV2(const char *name, BasicProperty *property_basic) {
+    Character *character = (Character *)malloc(sizeof(Character));
+    if (character == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    initCharacterV2(character, name, property_basic);
+    return character;
+}
+
+Character *createCharacterV3(const char *name, BasicProperty *property_basic, Knapsack *knapsack) {
+    Character *character = (Character *)malloc(sizeof(Character));
+    initCharacterV3(character, name, property_basic, knapsack);
     return character;
 }
 
--- src/character.h
+++ src/character.h
@@ -8,19 +8,19 @@
 
 typedef struct {
     char name[20];  // Character name
-    Weapon *weapon;  // Character weapon
+    Weapon *weapon; // Character weapon
     BasicProperty *property_basic;
     BattleProperty *property_fight;
-    Knapsack *knapsack;  // Character's knapsack
+    Knapsack *knapsack; // Character's knapsack
 } Character;
 
-void initCharacter(Character *character, const char *name, BasicProperty *property_basic, BattleProperty *property_fight);
-void initCharacterV2(Character *character, const char *name,
-                     BasicProperty *property_basic);
-void initCharacterV3(Character *character, const char *name,
-                     BasicProperty *property_basic, BattleProperty *property_fight,
-                     Knapsack *knapsack);
+void initCharacter(Character *character, const char *name, BasicProperty *property_basic,
+                   BattleProperty *property_fight);
+void initCharacterV2(Character *character, const char *name, BasicProperty *property_basic);
+void initCharacterV3(Character *character, const char *name, BasicProperty *property_basic, Knapsack *knapsack);
 Character *createCharacter(const char *name, BasicProperty *property_basic, BattleProperty *property_fight);
+Character *createCharacterV2(const char *name, BasicProperty *property_basic);
+Character *createCharacterV3(const char *name, BasicProperty *property_basic, Knapsack *knapsack);
 Character *createCharacterDeault();
 BasicProperty *getCharacterBasicProperty(Character *character);
 BattleProperty *getCharacterBattleProperty(Character *character);
