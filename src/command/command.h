49fc0da17cf1b947b868a722e0897e89703e2227
sleaf <huangzhiye2002@gmail.com>
2025-04-12 18:42:29 +0800

    添加攻击命令子类及其执行和撤销功能，更新命令基类以支持棋盘指针

--- src/command/command.h
+++ src/command/command.h
@@ -2,6 +2,7 @@
 #define COMMAND_H
 
 // 游戏操作类型（枚举）
+#include "chess_board.h"
 typedef enum {
     MOVE,
     ATTACK,
@@ -14,7 +15,8 @@ typedef struct Command {
     ActionType type;
     void (*execute)(struct Command*);  // 执行函数指针
     void (*undo)(struct Command*);     // 撤销函数指针
-    struct Command* next;              // 链表指针
+    struct Command *next;              // 链表指针
+    struct ChessBoard *board; // 棋盘指针
 } Command;
 
 #endif // COMMAND_H
